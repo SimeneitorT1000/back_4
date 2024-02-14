3d97f1f5663ca1c4c597618f29f0c5c8b3518717
SimeneitorT800 <simecristiano.m@gmail.com>
2024-02-14 11:49:58 -0600

    advance2

--- back4/src/routes/index.js
+++ back4/src/routes/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const routerUser = require('./user.router');
 const router = express.Router();
 
-// colocar las rutas aquí
+// las rutas van acá
+
 router.use('/users', routerUser)
 
 
-module.exports = router;
+module.exports = router; 
